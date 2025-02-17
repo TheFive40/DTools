41d3f30b281a4907fe75112131199197d3cc3c1d
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-02-17 09:25:31 -0400

    Agregados metodos esenciales

--- src/main/java/org/delaware/tools/Boosters/BonusAttributes.java
+++ src/main/java/org/delaware/tools/Boosters/BonusAttributes.java
@@ -1,4 +1,19 @@
 package org.delaware.tools.Boosters;
 
+import noppes.npcs.api.entity.IDBCPlayer;
+import noppes.npcs.scripted.NpcAPI;
+import org.bukkit.entity.Player;
+
 public class BonusAttributes {
+    IDBCPlayer player;
+    public BonusAttributes(Player player) {
+        this.player = NpcAPI.Instance().getPlayer(player.getName()).getDBCPlayer();
+    }
+    //STATS: str, dex, con, wil, spi ... bonusID: Nombre del boost (cualquiera) ... operation: +, -, *, /, %, value: valor
+    public void addBonus(String stat, String bonusID, String operation, double value) {
+        addBonus(stat, bonusID, operation, value, true);
+    }
+    public void addBonus(String stat, String bonusID, String operation, double value, boolean endOfLine) {
+        player.addBonusAttribute(stat, bonusID, operation, value, endOfLine);
+    }
 }
