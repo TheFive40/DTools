4157928f0d6bfb29eb4c717074b202684f79f76e
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-02-17 09:33:48 -0400

    Agregados metodos para eliminar bonus

--- src/main/java/org/delaware/tools/Boosters/BonusAttributes.java
+++ src/main/java/org/delaware/tools/Boosters/BonusAttributes.java
@@ -9,11 +9,24 @@ public class BonusAttributes {
     public BonusAttributes(Player player) {
         this.player = NpcAPI.Instance().getPlayer(player.getName()).getDBCPlayer();
     }
-    //STATS: str, dex, con, wil, spi ... bonusID: Nombre del boost (cualquiera) ... operation: +, -, *, /, %, value: valor
+    //STATS: str, dex, con, wil, spi ... bonusID: boost's id (any) ... operation: +, -, *, /, %
     public void addBonus(String stat, String bonusID, String operation, double value) {
         addBonus(stat, bonusID, operation, value, true);
     }
     public void addBonus(String stat, String bonusID, String operation, double value, boolean endOfLine) {
         player.addBonusAttribute(stat, bonusID, operation, value, endOfLine);
+    }
+    //Clears all the bonuses applied to the specified stat
+    public void clearBonus(String stat) {
+        player.clearBonusAttribute(stat);
+    }
+    //Clears all applied bonuses
+    public void clearAllBonuses() {
+        player.clearBonusAttribute("str");
+        player.clearBonusAttribute("dex");
+        player.clearBonusAttribute("con");
+        player.clearBonusAttribute("wil");
+        player.clearBonusAttribute("mnd");
+        player.clearBonusAttribute("spi");
     }
 }
