7920486eb58aabd12661860484ed93f84f8105ea
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-02-17 10:49:25 -0400

    Agregados metodos para verificar bonus

--- src/main/java/org/delaware/tools/Boosters/BonusAttributes.java
+++ src/main/java/org/delaware/tools/Boosters/BonusAttributes.java
@@ -1,13 +1,16 @@
 package org.delaware.tools.Boosters;
 
+import noppes.npcs.api.INbt;
 import noppes.npcs.api.entity.IDBCPlayer;
 import noppes.npcs.scripted.NpcAPI;
 import org.bukkit.entity.Player;
 
 public class BonusAttributes {
     IDBCPlayer player;
+    INbt nbt;
     public BonusAttributes(Player player) {
         this.player = NpcAPI.Instance().getPlayer(player.getName()).getDBCPlayer();
+        this.nbt = NpcAPI.Instance().getPlayer(player.getName()).getNbt().getCompound("PlayerPersisted");
     }
     //STATS: str, dex, con, wil, spi ... bonusID: boost's id (any) ... operation: +, -, *, /, %
     public void addBonus(String stat, String bonusID, String operation, double value) {
@@ -22,11 +25,20 @@ public class BonusAttributes {
     }
     //Clears all applied bonuses
     public void clearAllBonuses() {
-        player.clearBonusAttribute("str");
-        player.clearBonusAttribute("dex");
-        player.clearBonusAttribute("con");
-        player.clearBonusAttribute("wil");
-        player.clearBonusAttribute("mnd");
-        player.clearBonusAttribute("spi");
+        nbt.remove("jrmcAttrBonusstr");
+        nbt.remove("jrmcAttrBonusdex");
+        nbt.remove("jrmcAttrBonuscon");
+        nbt.remove("jrmcAttrBonuswil");
+        nbt.remove("jrmcAttrBonusmnd");
+        nbt.remove("jrmcAttrBonusspi");
+    }
+    //Checks for any bonus in the specified stat (regardless of bonusID)
+    public boolean hasAnyBonus(String stat) {
+        return nbt.has("jrmcAttrBonus" + stat);
+    }
+    //Checks if certain stat has an specific bonus
+    public boolean hasSpecificBonus(String stat, String bonusID) {
+        if(!hasAnyBonus(stat)) return false;
+        return nbt.getString("jrmcAttrBonus" + stat).contains(bonusID);
     }
 }
