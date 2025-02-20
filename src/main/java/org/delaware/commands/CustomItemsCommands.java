584803cda1c76cb3c4e5100a77ac3730292aef0b
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-02-20 01:00:26 -0400

    Added Remove and Get commands. Also made improvements to both CustomItemsCommands and CustomItems classes

--- src/main/java/org/delaware/commands/CustomItemsCommands.java
+++ src/main/java/org/delaware/commands/CustomItemsCommands.java
@@ -1,8 +1,8 @@
 package org.delaware.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.delaware.tools.CC;
 import org.delaware.tools.CustomItems.CustomItems;
 import org.delaware.tools.commands.BaseCommand;
@@ -20,7 +20,6 @@ public class CustomItemsCommands extends BaseCommand {
             player.sendMessage(command.getCommand().getUsage());
             return;
         }
-        Bukkit.getConsoleSender().sendMessage(args[0]);
         String action = args[0];
         switch(action.toLowerCase().trim()) {
             case "add":
@@ -28,15 +27,48 @@ public class CustomItemsCommands extends BaseCommand {
                     player.sendMessage(CC.translate("&cYou must be holding an item!"));
                     return;
                 }
+                if(args.length < 2) {
+                    player.sendMessage(CC.translate("&cYou must specify an ID!"));
+                    return;
+                }
                 CustomItems custom = new CustomItems(player.getItemInHand());
-                custom.addCustomItem("test");
+                custom.addCustomItem(args[1]);
                 break;
             case "list":
-                if(CustomItems.getAllCustomItems().isEmpty()) return;
+                if(CustomItems.getAllCustomItems().isEmpty()) {
+                    player.sendMessage(CC.translate("&cThere are no custom items registered"));
+                    return;
+                }
                 for(String item : CustomItems.getAllCustomItems()) {
                     player.sendMessage(CC.translate("&a" + item));
                 }
                 break;
+            case "get":
+                if(args.length < 2) {
+                    player.sendMessage(CC.translate("&cYou must specify an ID!"));
+                    return;
+                }
+                String key = args[1];
+                if(!CustomItems.contains(key)) {
+                    player.sendMessage(CC.translate("&cItem &4" + key + " &cdoes not exist!"));
+                    return;
+                }
+                ItemStack item = CustomItems.getCustomItem(key).toItemStack();
+                player.getInventory().addItem(item);
+                player.sendMessage(CC.translate("&2Given &a" + key + " &2to " + player.getName()));
+                break;
+            case "remove":
+                if(args.length < 2) {
+                    player.sendMessage(CC.translate("&cYou must specify an ID"));
+                    player.sendMessage(CC.translate("&cUse /dbCustomItems list to see all currently registered items"));
+                    return;
+                }
+                if(!CustomItems.contains(args[1])) {
+                    player.sendMessage(CC.translate("&c" + args[1] + " doesn't exist"));
+                    return;
+                }
+                CustomItems.removeItem(args[1]);
+                player.sendMessage(CC.translate("&a" + args[1] + " &2Deleted correctly"));
         }
     }
 }
--- src/main/java/org/delaware/tools/CustomItems/CustomItems.java
+++ src/main/java/org/delaware/tools/CustomItems/CustomItems.java
@@ -3,10 +3,10 @@ package org.delaware.tools.CustomItems;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftItemStack;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.delaware.tools.CC;
 
 import java.util.HashMap;
@@ -26,9 +26,6 @@ public class CustomItems {
     @Getter @Setter private Map<Enchantment, Integer> enchantments;
     @Getter @Setter private String nbtData;
 
-    public ItemStack toItemStack() {
-        return null;
-    }
     public CustomItems(ItemStack item) {
         this.displayName = item.getItemMeta().getDisplayName();
         this.lore = item.getItemMeta().getLore();
@@ -43,9 +40,21 @@ public class CustomItems {
             this.nbtData = null;
         }
     }
+    public ItemStack toItemStack() {
+        ItemStack itemStack = new ItemStack(itemID);
+        itemStack.setAmount(amount);
+        itemStack.setDurability(durability);
+        itemStack.addEnchantments(enchantments);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(displayName);
+        meta.setLore(lore);
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
     public void addCustomItem(String key) {
         items.put(key.toUpperCase().trim(), this);
     }
+    //STATIC METHODS
     public static CustomItems getCustomItem(String key) {
         try {
             return items.get(key.toUpperCase().trim());
@@ -57,4 +66,11 @@ public class CustomItems {
     public static Set<String> getAllCustomItems() {
         return items.keySet();
     }
+    public static boolean contains(String key) {
+        return items.containsKey(key);
+    }
+    public static void removeItem(String key) {
+        items.remove(key);
+    }
+    //STATIC METHODS
 }
