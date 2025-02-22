fad3f723e0d24e9b8d82e1336e8f29c29b7764f9
TheFive40 <jeanf2805@gmail.com>
2025-02-22 11:43:58 -0500

    "feat(commands): add command to remove all gifts"

---
+++ src/main/java/org/delaware/commands/CommandGiftRemovesAll.java
@@ -0,0 +1,22 @@
+package org.delaware.commands;
+
+import org.delaware.tools.commands.BaseCommand;
+import org.delaware.tools.commands.Command;
+import org.delaware.tools.commands.CommandArgs;
+
+import java.io.IOException;
+
+import static org.delaware.events.interactWithGift.*;
+
+public class CommandGiftRemovesAll extends BaseCommand {
+    @Command(name = "removegiftAll", description = "Remueve todos los regalos registrados", permission = "dtools.giftremoveall", aliases = "removegiftAll")
+    @Override
+    public void onCommand ( CommandArgs command ) throws IOException {
+        regalosEncontrados.clear ();
+        contadorRegalos.clear ();
+        misionCompletada.clear ( );
+        CommandAddGift.itemStackHashMap.clear ();
+        CommandAddGift.itemStacks.clear ();
+        command.getPlayer ( ).sendMessage ( "&aTodos los regalos han sido removidos exitosamente." );
+    }
+}
