5d5909f41500f6fef54479a0f3969425f2910dd6
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-03-11 18:00:42 -0400

    Added several classes to get Stat multipliers per race

---
+++ src/main/java/org/delaware/tools/Multipliers/Saiyan.java
@@ -0,0 +1,9 @@
+package org.delaware.tools.Multipliers;
+
+public class Saiyan {
+    public static final double strMultiplier = 3.5;
+    public static final double dexMultiplier = 3.4;
+    public static final double conMultiplier = 22.0;
+    public static final double wilMultiplier = 6.0;
+    public static final double spiMultiplier = 36.0;
+}
