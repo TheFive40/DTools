f74b8ae4e8e71e1ea650aaf75d28ac64bb82f414
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-03-15 21:30:32 -0400

    Added create item feature

--- src/main/java/org/delaware/tools/CustomItems/Inventories/MainMenu.java
+++ src/main/java/org/delaware/tools/CustomItems/Inventories/MainMenu.java
@@ -11,15 +11,17 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.delaware.Main;
 import org.delaware.tools.CC;
 import org.delaware.tools.CustomItems.CustomItems;
 import org.delaware.tools.General;
+import org.delaware.tools.input.PlayerInput;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class MainMenu implements InventoryProvider {
-    private ArrayList<CustomItems> allItems;
+    private final ArrayList<CustomItems> allItems;
     public MainMenu(@NonNull ArrayList<CustomItems> allItems) {
         this.allItems = allItems;
     }
@@ -50,10 +52,15 @@ public class MainMenu implements InventoryProvider {
         ItemMeta metaN = nextPageItem.getItemMeta();
         metaN.setDisplayName(CC.translate("&fSiguiente página"));
         nextPageItem.setItemMeta(metaN);
+        ItemStack createItem = new ItemStack(Material.DIAMOND_BLOCK);
+        ItemMeta createMeta = createItem.getItemMeta();
+        createMeta.setDisplayName(CC.translate("&aCrear nuevo item"));
+        createItem.setItemMeta(createMeta);
         inventoryContents.set(5, 2, ClickableItem.of(previousPageItem,
                 e -> inventoryContents.inventory().open(player, pagination.previous().getPage())));
         inventoryContents.set(5, 6, ClickableItem.of(nextPageItem,
                 e -> inventoryContents.inventory().open(player, pagination.next().getPage())));
+        inventoryContents.set(0, 8, ClickableItem.of(createItem, e -> createItem(player, inventoryContents.inventory())));
     }
 
     @Override
@@ -61,5 +68,31 @@ public class MainMenu implements InventoryProvider {
     private void itemClick(Player player, CustomItems item) {
         final SmartInventory NEW_INV = SmartInventory.builder().provider(new ConfigMenu(item)).size(6, 9).id("ItemsConfig").title(CC.translate("&eConfiguración")).build();
         NEW_INV.open(player);
+    }
+    private void createItem(Player player, SmartInventory inventory) {
+        player.sendMessage(CC.translate("&6------------------------"));
+        player.sendMessage(CC.translate("&eIngresa la ID del item a crear"));
+        player.sendMessage(CC.translate("&eNOTA: Debes tener un item en la mano"));
+        player.sendMessage(CC.translate("&6------------------------"));
+        inventory.close(player);
+        PlayerInput input = new PlayerInput(player, Main.instance);
+        input.waitForPlayerInput(30, "&cTiempo de espera agotado", ID -> {
+            if(player.getItemInHand().getType().equals(Material.AIR)) {
+                player.sendMessage(CC.translate("&cDebes tener un item en la mano!"));
+                inventory.open(player);
+                return;
+            }
+            if(ID.contains(" ")) ID = ID.replace(" ", "");
+            if(CustomItems.contains(ID.toUpperCase().trim())) {
+                player.sendMessage(CC.translate("&cItem con ID " + ID + " ya existe en la config!"));
+                inventory.open(player);
+                return;
+            }
+            CustomItems add = new CustomItems(player.getItemInHand());
+            add.addCustomItem(ID.toUpperCase().trim());
+            player.sendMessage(CC.translate("&aItem agregado correctamente"));
+            allItems.add(CustomItems.getCustomItem(ID));
+            inventory.open(player);
+        });
     }
 }
